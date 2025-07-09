04eb488434fef9bc8bb679fec04b7cfcfbda2cc5
COCOVIVEN <914604109@qq.com>
2025-07-09 18:44:04 +0800

    class007--结构体

--- class007/StructType.cpp
+++ class007/StructType.cpp
@@ -20,19 +20,31 @@ private: // 可以定义为私有
 struct Class{
     int id;
     int grade;
-private:
-    int num;
 };
+
+typedef struct{
+    int id;
+    std::string name;
+} StudentType; // StudentType定义为结构体
+
+using StudentType2 = struct{
+    int id;
+    std::string name;
+}; // StudentType2定义为结构体
 
 int main(){
     // 无成员函数，且无私有成员变量的结构体，可用花括号直接赋初值
-    Class c1;
+    Class c1 = {1,2};
 
     Student s1;
     s1.age = 18;
     s1.name = "张三";
     s1.score = 100.0;
     s1.setId(1);
+
+    StudentType s2 = {2, "李四"};
+
+    StudentType2 s3 = {3, "王五"};
 
     system("pause");
     return 0;
